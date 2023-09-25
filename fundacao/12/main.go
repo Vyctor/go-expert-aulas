24fd9acd3488d5003c8b4830be0863aefd3b808d
Vyctor <dev.vyctor@gmail.com>
2023-09-24 23:12:59 -0300

    composicao de structs

--- fundacao/12/main.go
+++ fundacao/12/main.go
@@ -2,19 +2,10 @@ package main
 
 import "fmt"
 
-type Endereco struct {
-	Logradouro string
-	Numero     int
-	Bairro     string
-	Cidade     string
-	Estado     string
-}
-
 type Cliente struct {
 	Nome  string
 	Idade int
 	Ativo bool
-	Endereco
 }
 
 func main() {
@@ -22,13 +13,6 @@ func main() {
 		Nome:  "Vyctor",
 		Idade: 28,
 		Ativo: true,
-		Endereco: Endereco{
-			Logradouro: "Rua dos Bobos",
-			Numero:     0,
-			Bairro:     "Centro",
-			Cidade:     "São Paulo",
-			Estado:     "SP",
-		},
 	}
 
 	fmt.Printf("Nome: %s\nIdade: %d\nAtivo: %t\n", vyctor.Nome, vyctor.Idade, vyctor.Ativo)
