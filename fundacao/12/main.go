adb8b5e964310a63f59a5c22f3b994640995b63b
Vyctor <dev.vyctor@gmail.com>
2023-09-24 23:53:29 -0300

    ponteiros e structs

--- fundacao/12/main.go
+++ fundacao/12/main.go
@@ -1,14 +1,44 @@
 package main
 
-func soma(a, b *int) int {
-	*a = 50
-	return *a + *b
+import "fmt"
+
+type Cliente struct {
+	nome string
+}
+
+type Conta struct {
+	saldo int
+}
+
+func NewConta() *Conta {
+	return &Conta{saldo: 0}
+}
+
+func (c *Cliente) andou() {
+	c.nome = "Vyctor Guimarães"
+	fmt.Printf("O cliente %v andou\n", c.nome)
+}
+
+func (c Conta) simular(valor int) int {
+	c.saldo += valor
+	return c.saldo
+}
+
+func (c *Conta) depositar(valor int) int {
+	c.saldo += valor
+	return c.saldo
 }
 
 func main() {
-	minhaVar1 := 10
-	minhaVar2 := 20
+	vyctor := Cliente{
+		nome: "Vyctor",
+	}
+	vyctor.andou()
+	fmt.Printf("O valor da struct com nome é %v\n", vyctor.nome)
 
-	println(soma(&minhaVar1, &minhaVar2))
-	println(minhaVar1)
+	conta := NewConta()
+	conta.simular(200)
+	println(conta.saldo)
+	conta.depositar(1000)
+	println(conta.saldo)
 }
