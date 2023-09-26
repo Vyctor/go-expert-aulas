175eba843079a274d5cec59f34cce83adc5a25b1
Vyctor <dev.vyctor@gmail.com>
2023-09-25 22:06:41 -0300

    instalando pacotes

--- fundacao/18/main.go
+++ fundacao/18/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 
+	"github.com/google/uuid"
 	"github.com/vyctor/curso-go/matematica"
 )
 
 func main() {
 	soma := matematica.Soma(10, 10)
 	fmt.Println("Resultado: ", soma)
+	fmt.Println("UUID: ", uuid.New())
 }
