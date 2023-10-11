2f829f7d2f6d22614a43ac15bd2e18bfa83502ee
Vyctor <dev.vyctor@gmail.com>
2023-10-10 23:12:35 -0300

    manipulando arquivos

---
+++ pacotes-importantes/1-manipulacao-de-arquivos/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	// Criação e escrita
+	f, error := os.Create("arquivo.txt")
+
+	if error != nil {
+		panic(error)
+	}
+
+	//tamanho, error := f.WriteString("Olá, mundo!")
+	tamanho, error := f.Write([]byte("Hello, world!"))
+
+	if error != nil {
+		panic(error)
+	}
+
+	fmt.Printf("Tamanho: %d bytes\n", tamanho)
+
+	defer f.Close()
+
+	// leitura
+	arquivo, error := os.ReadFile("arquivo.txt")
+	if error != nil {
+		panic(error)
+	}
+
+	fmt.Println(string(arquivo))
+
+	// leitura de pouco em pouco abrindo o arquivo
+	arquivo2, error := os.Open("arquivo.txt")
+
+	if error != nil {
+		panic(error)
+	}
+
+	reader := bufio.NewReader(arquivo2)
+	buffer := make([]byte, 3)
+
+	for {
+		n, err := reader.Read(buffer)
+		if err != nil {
+			break
+		}
+
+		fmt.Println(string(buffer[:n]))
+	}
+
+}
