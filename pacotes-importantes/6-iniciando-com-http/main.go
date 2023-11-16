d546a3aa441293a905baff257513d59e275cb3e7
Vyctor <dev.vyctor@gmail.com>
2023-11-15 22:15:01 -0300

    criando e finalizando funcao busca cep

--- pacotes-importantes/6-iniciando-com-http/main.go
+++ pacotes-importantes/6-iniciando-com-http/main.go
@@ -1,6 +1,23 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+type ViaCEP struct {
+	Cep         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	Uf          string `json:"uf"`
+	Ibge        string `json:"ibge"`
+	Gia         string `json:"gia"`
+	Ddd         string `json:"ddd"`
+	Siafi       string `json:"siafi"`
+}
 
 func main() {
 	http.HandleFunc("/", BuscaCepHandler)
@@ -12,15 +29,38 @@ func BuscaCepHandler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
-
 	cepParam := r.URL.Query().Get("cep")
-
 	if cepParam == "" {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
+	data, err := BuscaCep(cepParam)
 
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	response, err := json.Marshal(data)
 	w.Header().Set("Content-Type", "application/json charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Olá mundo!"))
+	w.Write(response)
+}
+
+func BuscaCep(cep string) (*ViaCEP, error) {
+	response, error := http.Get("http://viacep.com.br/ws/" + cep + "/json/")
+	if error != nil {
+		return nil, error
+	}
+	defer response.Body.Close()
+	body, error := io.ReadAll(response.Body)
+	if error != nil {
+		return nil, error
+	}
+	var data ViaCEP
+	error = json.Unmarshal(body, &data)
+	if error != nil {
+		return nil, error
+	}
+	return &data, nil
+
 }
