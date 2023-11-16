dbd4cb2bc31568326f13819e4d4262dad4a43aad
Vyctor <dev.vyctor@gmail.com>
2023-11-15 22:03:23 -0300

    manipulando headers

--- pacotes-importantes/6-iniciando-com-http/main.go
+++ pacotes-importantes/6-iniciando-com-http/main.go
@@ -3,10 +3,24 @@ package main
 import "net/http"
 
 func main() {
-	http.HandleFunc("/", BuscaCEP)
+	http.HandleFunc("/", BuscaCepHandler)
 	http.ListenAndServe(":8000", nil)
 }
 
-func BuscaCEP(w http.ResponseWriter, r *http.Request) {
+func BuscaCepHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	cepParam := r.URL.Query().Get("cep")
+
+	if cepParam == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json charset=utf-8")
+	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Olá mundo!"))
 }
