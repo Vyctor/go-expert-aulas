47c88a2edaf2d69e0d59acdcdc26e5fb4d7e8eca
Vyctor <dev.vyctor@gmail.com>
2023-11-15 22:36:59 -0300

    template.must

---
+++ pacotes-importantes/8-templates/2/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"os"
+	"text/template"
+)
+
+type Curso struct {
+	Nome         string
+	CargaHoraria int
+}
+
+func main() {
+	curso := Curso{"Go", 40}
+	t := template.Must(template.New("CursoTemplate").Parse("Curso: {{.Nome}}\nCarga Horária: {{.CargaHoraria}} horas"))
+
+	err := t.Execute(os.Stdout, curso)
+
+	if err != nil {
+		panic(err)
+	}
+}
