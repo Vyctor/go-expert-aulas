7ba64ff935f5fe21fc9c7e42343a5226b072e320
Vyctor <dev.vyctor@gmail.com>
2023-11-15 23:09:35 -0300

    templates com arquivo externo

---
+++ pacotes-importantes/8-templates/3/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"os"
+	"text/template"
+)
+
+type Curso struct {
+	Nome         string
+	CargaHoraria int
+}
+
+type Cursos []Curso
+
+func main() {
+	t := template.Must((template.New("template.html").ParseFiles("./template.html")))
+
+	err := t.Execute(os.Stdout, Cursos{
+		Curso{"Go", 40},
+		Curso{"Python", 35},
+		Curso{"C#", 50},
+		Curso{"Java", 30},
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+}
