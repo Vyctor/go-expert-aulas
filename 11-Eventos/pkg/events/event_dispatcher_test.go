10c8c2e89f4e3cde7581a74aa2fd10aee3d02e4a
Vyctor <dev.vyctor@gmail.com>
2024-05-01 17:18:49 -0300

    Criando suite de testes

---
+++ 11-Eventos/pkg/events/event_dispatcher_test.go
@@ -0,0 +1,66 @@
+package events
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+)
+
+type TestEvent struct {
+	Name    string
+	Payload interface{}
+}
+
+func (e TestEvent) GetName() string {
+	return e.Name
+}
+
+func (e TestEvent) GetPayload() interface{} {
+	return e.Payload
+}
+
+func (e TestEvent) GetDateTime() time.Time {
+	return time.Now()
+}
+
+type TestEventHandler struct {
+}
+
+func (h TestEventHandler) Handle(event EventInterface) {
+
+}
+
+type EventDispatcherTestSuit struct {
+	suite.Suite
+	event           TestEvent
+	event2          TestEvent
+	handler         TestEventHandler
+	handler2        TestEventHandler
+	handler3        TestEventHandler
+	eventDispatcher *EventDispatcher
+}
+
+func (suite *EventDispatcherTestSuit) SetupTest() {
+	suite.eventDispatcher = NewEventDispatcher()
+	suite.handler = TestEventHandler{}
+	suite.handler2 = TestEventHandler{}
+	suite.handler3 = TestEventHandler{}
+	suite.event = TestEvent{
+		Name:    "test",
+		Payload: "test",
+	}
+	suite.event2 = TestEvent{
+		Name:    "test2",
+		Payload: "test2",
+	}
+}
+
+func (suite *EventDispatcherTestSuit) TestEventDispatcher_Register() {
+	assert.True(suite.T(), true)
+}
+
+func TestSuite(t *testing.T) {
+	suite.Run(t, new(EventDispatcherTestSuit))
+}
