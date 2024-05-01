ef7b2d8fb4e95bae9554998c3985b481ff706f97
Vyctor <dev.vyctor@gmail.com>
2024-05-01 17:27:10 -0300

    Testando Register

--- 11-Eventos/pkg/events/event_dispatcher_test.go
+++ 11-Eventos/pkg/events/event_dispatcher_test.go
@@ -26,6 +26,7 @@ func (e TestEvent) GetDateTime() time.Time {
 }
 
 type TestEventHandler struct {
+	ID int
 }
 
 func (h TestEventHandler) Handle(event EventInterface) {
@@ -44,9 +45,15 @@ type EventDispatcherTestSuit struct {
 
 func (suite *EventDispatcherTestSuit) SetupTest() {
 	suite.eventDispatcher = NewEventDispatcher()
-	suite.handler = TestEventHandler{}
-	suite.handler2 = TestEventHandler{}
-	suite.handler3 = TestEventHandler{}
+	suite.handler = TestEventHandler{
+		ID: 1,
+	}
+	suite.handler2 = TestEventHandler{
+		ID: 2,
+	}
+	suite.handler3 = TestEventHandler{
+		ID: 3,
+	}
 	suite.event = TestEvent{
 		Name:    "test",
 		Payload: "test",
@@ -58,7 +65,21 @@ func (suite *EventDispatcherTestSuit) SetupTest() {
 }
 
 func (suite *EventDispatcherTestSuit) TestEventDispatcher_Register() {
-	assert.True(suite.T(), true)
+	err := suite.eventDispatcher.Register(suite.event.GetName(), suite.handler)
+	suite.Nil(err)
+	suite.Equal(1, len(suite.eventDispatcher.handlers[suite.event.GetName()]))
+
+	err = suite.eventDispatcher.Register(suite.event.GetName(), suite.handler2)
+	suite.Nil(err)
+	suite.Equal(2, len(suite.eventDispatcher.handlers[suite.event.GetName()]))
+
+	err = suite.eventDispatcher.Register(suite.event.GetName(), suite.handler3)
+	suite.Nil(err)
+	suite.Equal(3, len(suite.eventDispatcher.handlers[suite.event.GetName()]))
+
+	assert.Equal(suite.T(), suite.handler, suite.eventDispatcher.handlers[suite.event.GetName()][0])
+	assert.Equal(suite.T(), suite.handler2, suite.eventDispatcher.handlers[suite.event.GetName()][1])
+	assert.Equal(suite.T(), suite.handler3, suite.eventDispatcher.handlers[suite.event.GetName()][2])
 }
 
 func TestSuite(t *testing.T) {
